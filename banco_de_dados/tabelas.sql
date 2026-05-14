d3cc058a4bfab0c75570fb5a9c4eaf3624f0ddda
Lucas3255 <lucashenrique102006@gmail.com>
2026-05-14 13:39:26 -0300

    update de infos de tabelas

--- banco_de_dados/tabelas.sql
+++ banco_de_dados/tabelas.sql
@@ -1,13 +1,13 @@
 CREATE DATABASE gestao_academica;
 
-CREATE TABLE cursos (	--este comando é o auto increment.
-	id_curso INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+CREATE TABLE cursos (
+	id_curso SERIAL PRIMARY KEY,
 	nome_curso VARCHAR(50),
-	carga_horaria INTEGER
+	carga_horaria INT
 );
 
 CREATE TABLE alunos (
-	id_aluno INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	id_aluno SERIAL PRIMARY KEY,
 	nome VARCHAR(100),
 	cpf VARCHAR(15),
 	email VARCHAR(60),
@@ -15,38 +15,38 @@ CREATE TABLE alunos (
 );
 
 CREATE TABLE professores (
-	id_professor INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	id_professor SERIAL PRIMARY KEY,
 	nome VARCHAR(100),
 	email VARCHAR(60),
 	especializacao VARCHAR(50)
 );
 
 CREATE TABLE disciplinas (
-	id_disciplina INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	id_disciplina SERIAL PRIMARY KEY,
 	nome VARCHAR(100),
-	carga_horaria INTEGER,
-	id_curso INTEGER,
+	carga_horaria INT,
+	id_curso INT,
 	
 	FOREIGN KEY (id_curso) REFERENCES cursos(id_curso)
 );
 
 CREATE TABLE turmas (
-	id_turma INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	id_turma SERIAL PRIMARY KEY,
 	semestre VARCHAR(20),
 	turno VARCHAR(10),
-	id_disciplina INTEGER,
-	id_professor INTEGER,
+	id_disciplina INT,
+	id_professor INT,
 	
 	FOREIGN KEY (id_disciplina) REFERENCES disciplinas (id_disciplina),
 	FOREIGN KEY (id_professor) REFERENCES professores (id_professor)
 );
 
 CREATE TABLE matriculas (
-	id_matricula INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	id_matricula SERIAL PRIMARY KEY,
 	status VARCHAR(10),
 	data_matricula DATE,
-	id_turma INTEGER,
-	id_aluno INTEGER,
+	id_turma INT,
+	id_aluno INT,
 	
 	FOREIGN KEY (id_turma) REFERENCES turmas(id_turma),
 	FOREIGN KEY (id_aluno) REFERENCES alunos (id_aluno)
